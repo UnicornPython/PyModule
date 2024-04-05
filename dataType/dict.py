f5ff1c8ad98a3a5829d5aba5621a4fdf4240c98e
ancion <ancion.github.com>
2024-04-05 16:02:32 +0800

    f string

--- dataType/dict.py
+++ dataType/dict.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
 
+import re
+
 
 def dict():
-    ...
+    exp = re.compile("[A-Za-z0-9_]+")
+    val = exp.findall("hello 324234")
+    print(val)
 
 
 def main():
-    ...
+    dict()
 
 
 if __name__ == "__main__":
     main()
+
--- dataType/string.py
+++ dataType/string.py
@@ -6,8 +6,36 @@ python 中的字符串
 """
 
 
+from datetime import datetime
+
+
+def f_template():
+
+    # 1.空白字符对齐
+    var: str = "hello"
+    # 对齐 20 个字符, 右对齐
+    print(f"{var:>20}")
+    # 对齐 20 个字符，左对齐
+    print(f"{var:<20}")
+    # 中间对齐
+    print(f"{var:^20}")
+    # 指定对齐的字符
+    print(f"{var:#>20}")
+
+    # 2.数字
+    num: int = 100000000
+    # 数字显示(指定分割符号)
+    print(f"{num:_}")
+    print(f"{num:,}")
+
+    # 3. 日期
+    date: datetime = datetime.now()
+    print(f"{date:%c}")
+    
+
+
 def main():
-    ...
+    f_template()
 
 
 if __name__ == "__main__":
